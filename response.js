8ed30132613d687ab9776d774fe372bdcdf0009c
ramcharan16 <109079783+ramcharan16@users.noreply.github.com>
2023-08-25 11:18:44 +0530

    Create response.js
    the response of app.js is processed according to incoming requests

---
+++ response.js
@@ -0,0 +1,12 @@
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next(); //Allows the request to continue to the next middleware in line
+});
+
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!<h1>');
+    
+});
+app.listen(3000)
+ 
